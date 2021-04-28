f0d65b8d4f261d24949f07c074d1c9a89bc1529e
Matheus S. Silva <teus_26@hotmail.com>
2021-04-28 16:06:05 -0300

    Create EX19.c

---
+++ EX19.c
@@ -0,0 +1,45 @@
+//Calculadora com soma, subtração, multiplicação e divisão.
+
+#include <stdio.h>
+
+int main()
+{
+  float numero1, numero2, resultado;
+  char operacao;
+
+  printf("Digite o primeiro número: ");
+  scanf("%f", &numero1);
+
+  printf("Digite a operação: ");
+  scanf("%s", &operacao);
+
+  printf("Digite o segundo número: ");
+  scanf("%f", &numero2);
+
+  switch(operacao)
+  {
+    case '+':
+    resultado = numero1+numero2;
+    break;
+
+    case '-':
+    resultado = numero1-numero2;
+    break;
+
+    case '*':
+    resultado = numero1*numero2;
+    break;
+
+    case '/':
+    resultado = numero1/numero2;
+    break;
+
+    default:
+    printf("ERRO!");
+  }
+
+  printf("Resultado: %.2f", resultado);
+
+
+  return 0;
+}
