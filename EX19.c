46b819697979db48b2b9c2637d956c48cb61c085
Matheus S. Silva <teus_26@hotmail.com>
2021-04-28 16:15:03 -0300

    Update EX19.c

--- EX19.c
+++ EX19.c
@@ -35,7 +35,7 @@ int main()
     break;
 
     default:
-    printf("ERRO! opção inválida, tente: + - * /");
+    printf("ERRO! operação inválida, tente: + - * /");
   }
 
   printf("Resultado: %.2f", resultado);
