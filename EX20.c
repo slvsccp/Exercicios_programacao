873b84b7808a7a3f5dc4c7e226281b6f6b712e23
Matheus S. Silva <teus_26@hotmail.com>
2021-05-10 17:03:26 -0300

    Create EX20.c

---
+++ EX20.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+  float valorDigitado, soma;
+
+  printf("Digite um valor para a soma: ");
+  scanf("%f", &valorDigitado);
+
+  while(valorDigitado != 0){
+    soma += valorDigitado;
+    printf("\nTotal: %.2f", soma);
+
+    printf("\nDigite um valor para a soma: ");
+    scanf("%2f", &valorDigitado);
+  }
+  printf("\nResultado: %.2f", soma);
+
+  return 0;
+}
